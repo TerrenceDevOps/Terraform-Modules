069ba234cc5f6512c2c02a5a7d4978bde6058a0b
Terrence Ncube <terrencencube593@gmail.com>
2025-04-20 19:26:58 +0200

    s3

--- s3/main.tf
+++ s3/main.tf
@@ -3,13 +3,13 @@ resource "aws_s3_bucket" "env_file_bucket" {
   bucket = "${var.project_name}-${var.env_file_bucket_name}"
 
   lifecycle {
-    create_before_destroy = 
+    create_before_destroy = false
   }
 }
 
 # upload the environment file from local computer into the s3 bucket
 resource "aws_s3_object" "upload_env_file" {
-  bucket = 
-  key    = 
-  source = "./${}"
+  bucket = aws_s3_object.env_file_bucket.id
+  key    = var.env_file_name
+  source = "./${var.env_file_name}"
 }
