2dcbfba7976e420e8385354b40136f3f68754e38
Terrence Ncube <terrencencube593@gmail.com>
2025-04-20 09:03:38 +0200

    commit

--- rds/variables.tf
+++ rds/variables.tf
@@ -6,6 +6,6 @@ variable "private_data_subnet_az2_id" {}
 variable "database_snapshot_identifier" {}
 variable "database_instance_class" {}
 variable "availability_zone_1" {}
-variable "database_snapshot_identifier" {}
+variable "database_instance_identifier" {}
 variable "multi_az_deployment" {}
 variable "database_security_group_id" {}
